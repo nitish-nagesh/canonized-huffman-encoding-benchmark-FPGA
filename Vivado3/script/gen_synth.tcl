ef1b5e9ecab80352d7f2764b9ed5371aff99eb1b
John So <herezjonnie@gmail.com>
2020-03-17 20:54:01 -0700

    Added Vivado3. Same as original design, but with a different target device

---
+++ Vivado3/script/gen_synth.tcl
@@ -0,0 +1,16 @@
+open_project huffman_encoding
+set_top huffman_encoding
+add_files huffman.h
+add_files {huffman_canonize_tree.cpp huffman_create_tree.cpp huffman_filter.cpp huffman_compute_bit_length.cpp huffman_encoding.cpp huffman_sort.cpp huffman_create_codeword.cpp huffman_truncate_tree.cpp}
+add_files huffman_params.h
+add_files -tb huffman_encoding_test.cpp
+add_files -tb {huffman.random256.txt huffman.random256.golden}
+
+# Solution
+open_solution -reset "solutionx"
+set_part {xcvu9pflga-2104-1-e} -tool vivado
+create_clock -period 10
+csim_design
+csynth_design
+
+exit
---
+++ Vivado3/src/huffman_params.h
@@ -0,0 +1,17 @@
+#ifndef PARAMS_H_
+#define PARAMS_H_
+
+#define PRAGMA_SUB(x) _Pragma (#x)
+#define PRAGMA_HLS(x) PRAGMA_SUB(x)
+
+#define copy0 1
+#define copy1 1
+#define computehistogram 1
+#define computehistogram_partition 1
+#define resort 1
+#define processsymbols 1
+#define movenodes 1
+#define assigncodeword 1
+
+
+#endif
